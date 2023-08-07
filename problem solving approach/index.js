1c0b42bb8c84a454f3197248f09b655385ecfe55
developer-sujon <developersujon@yahoo.com>
2023-08-07 11:12:14 +0600

    how to understand and write sudo code

---
+++ problem solving approach/index.js
@@ -0,0 +1,10 @@
+//sum 2 numbers and return the result
+//addition two numbers
+//input - 5, 8 //output - 12
+//input - 10, 20 //output - 30
+
+function twoSum(num1, num2) {
+  return num1 + num2;
+}
+
+twoSum(10, 20);
